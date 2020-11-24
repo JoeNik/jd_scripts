07e0ed20564f9e4c14d6453c4887d67b061c09ff
JoeNik <154469502@qq.com>
2020-11-24 15:38:43 +0800

    Update jd_small_home.js

--- jd_small_home.js
+++ jd_small_home.js
@@ -427,7 +427,8 @@ function createInviteUser() {
             if (data.head.code === 200) {
               if (data.body) {
                 if (data.body.id) {
-				  myCode.push(shareCode);
+		  myCode.push(data.body.id);
+		  console.log('code lenth:'|| myCode.length);
                   console.log(`\n您的${$.name}shareCode(每天都是变化的):【${data.body.id}】\n`);
                   $.shareCode = data.body.id;
                 }
